92415217bada7fc2aadfae70a8327a8df6f974c7
cosiaca350 <cosiaca350@gmail.com>
2025-11-08 13:57:05 -0500

    Updated index.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -74,10 +74,11 @@ const Navbar = ({ view, setView }) => {
                     onKeyDown={(e) => e.key === 'Enter' && setView('home')}
                     aria-label="Ir al inicio"
                 >
-                    <img 
-                        src="/logo-cosiaca.svg" 
+                    <img
+                        src="/logo-cosiaca.svg"
                         alt="Cosiaca 350"
-                        className="h-8 sm:h-10 md:h-12 w-auto"
+                        className="h-8 sm:h-10 md:h-12 w-auto object-contain"
+                        style={{ minWidth: '120px' }}
                     />
                 </div>
                 
